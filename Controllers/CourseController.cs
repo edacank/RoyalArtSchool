7d0550da39906a59599a12f9bcd4d74e2ca87d45
edacank <edacan1924@gmail.com>
2024-12-31 14:17:52 +0300

    summary

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoyalArtSchool.Models;
+
+namespace RoyalArtSchool.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //get için çalışan methodur. [HttpGet] yazmasa da defaultu gettit
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm]Candidate model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Candidate.cs
@@ -0,0 +1,22 @@
+﻿namespace RoyalArtSchool.Models
+{
+    public class Candidate
+    {
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public int? Age { get; set; }
+        //? ile nullcheck yaptım.
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public String? Email { get; set; }=String.Empty;
+        public String? SelectedCourseName {  get; set; }= String.Empty;
+        public DateTime AppliedAt { get; set; }
+        //bunu yukarıda yapabilirdim ama nesne tanımlanırken yaptım.
+        public Candidate()
+        {
+            AppliedAt = DateTime.Now;
+        }
+
+
+
+    }
+}
